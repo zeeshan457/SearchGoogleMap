d690b0f3da5d033d7f5ddbb2f4f353cce161d49c
zeeshan <zeeshanm2405@gmail.com>
2023-01-10 23:40:31 +0000

    final commit
    code improvements and docs

--- SearchGoogleMap/app/src/main/java/com/example/searchgooglemap/MapsActivty.java
+++ SearchGoogleMap/app/src/main/java/com/example/searchgooglemap/MapsActivty.java
@@ -2,15 +2,28 @@ package com.example.searchgooglemap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.SearchView;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivty extends AppCompatActivity {
+import java.io.IOException;
+import java.util.List;
+
+public class MapsActivty extends AppCompatActivity implements OnMapReadyCallback {
 
     /**
      *
@@ -18,12 +31,10 @@ public class MapsActivty extends AppCompatActivity {
      *
      */
 
-    private View map;
-    private ImageView search;
+    private SearchView search;
     private Button logoutButton;
-    private Boolean initialiseMap;
-    private EditText location;
     private GoogleMap map;
+    private SupportMapFragment fragment;
 
 
     /**
@@ -37,15 +48,15 @@ public class MapsActivty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.maps);
-        search = findViewById(R.id.searchButton);
         logoutButton = findViewById(R.id.logoutButton);
-        location = findViewById(R.id.location);
+        search = findViewById(R.id.searchView);
+        fragment = SupportMapFragment.newInstance();
+        getSupportFragmentManager().beginTransaction().add(R.id.googleMap, fragment).commit();
+        fragment.getMapAsync(this);
 
-        checkPermissions();
+        actionEvents();
 
-
-
-
+       // onMapReady(googleMap);
     }
 
 
@@ -57,16 +68,51 @@ public class MapsActivty extends AppCompatActivity {
      *
      */
     public void actionEvents() {
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivty.class);
+                startActivity(intent);
+            }
+        });
 
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                String findLocation = search.getQuery().toString();
+                List<Address> addressList = null;
+                if (findLocation != null || !findLocation.equals("")) {
+                    Geocoder geo = new Geocoder(MapsActivty.this);
+
+                        try {
+                            addressList = geo.getFromLocationName(findLocation, 1);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        Address address = addressList.get(0);
+                    LatLng data = new LatLng(address.getLatitude(), address.getLongitude());
+                    map.addMarker(new MarkerOptions().position(data).title(findLocation));
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(data, 10));
+
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        fragment.getMapAsync(this);
 
     }
 
-    public void mapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.map = googleMap;
     }
-
-
-
 }
